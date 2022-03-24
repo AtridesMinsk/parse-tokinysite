2c130ff155d6cb5cd36c364e1406d96fc2f12bdc
pbarabanau <atrides@gmail.com>
2022-03-24 15:33:59 +0300

    Ver 0.0.2 Added: Parse by Scrapy framework

--- Scrapy/parse_tokiny/tokiny/items.py
+++ Scrapy/parse_tokiny/tokiny/items.py
@@ -6,7 +6,9 @@
 import scrapy
 
 
-class TokinyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Product(scrapy.Item):
+    title = scrapy.Field()
+    price = scrapy.Field()
+    images = scrapy.Field()
+    description = scrapy.Field()
+    product_url = scrapy.Field()
--- Scrapy/parse_tokiny/tokiny/spiders/tokiny_menu.py
+++ Scrapy/parse_tokiny/tokiny/spiders/tokiny_menu.py
@@ -1,10 +1,31 @@
 import scrapy
+import requests
+
+from bs4 import BeautifulSoup
+from scrapy import Item, Field
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from tokiny.items import Product
 
 
-class TokinyMenuSpider(scrapy.Spider):
+class MenuSpider(CrawlSpider):
     name = 'tokiny_menu'
-    allowed_domains = ['parse_tokiny.by']
-    start_urls = ['http://tokiny.by/']
+    allowed_domains = ['tokiny.by']
+    start_urls = ['https://tokiny.by/']
 
-    def parse(self, response):
-        pass
+    rules = (
+        Rule(LinkExtractor(allow=('/menyu/',),
+                           deny=('index.php', 'search', 'tag', 'revblog_blog', 'jpg', 'png', 'page',
+                                 'uploads', 'autor', 'simpleregister', 'my_account',)),
+             callback='parse', follow=True),
+
+    )
+
+    def parse(self, response, **kwargs):
+        item = Product()
+        item['title'] = response.xpath('//h1/text()').get()
+        item['price'] = response.xpath('//div[@class="price"]//span/text()').get()
+        item['description'] = response.xpath('//div[@class="item-popup-description"]/text()').get()
+        item['product_url'] = response.url
+        item['images'] = response.xpath('//div[@class="big-img-holder"]//url/@src').get()
+        yield item
